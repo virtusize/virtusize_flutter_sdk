ae76e7f440e5c9f570a83170bad889d9776e0ca0
Kuei <akueisara39@gmail.com>
2021-07-05 18:31:31 +0900

    Implement the method call getRecommendationText for iOS

--- ios/Classes/FlutterErrors.swift
+++ ios/Classes/FlutterErrors.swift
@@ -1,7 +1,15 @@
 import Flutter
 
-internal let FlutterMethodNoArguments = FlutterError(code: "-1", message: "Missing arguments.", details: nil)
-
-internal func FlutterMethodNotSetArgument(_ arg: String) -> FlutterError {
-   return FlutterError(code: "-1", message: "\(arg) is not set.", details: nil)
+extension FlutterError {
+	internal static let noArguments = FlutterError(code: "-1", message: "Missing arguments.", details: nil)
+	
+	internal static func argumentNotSet(_ arg: String) -> FlutterError {
+		return FlutterError(code: "-1", message: "\(arg) is not set.", details: nil)
+	}
+	
+	internal static func nullAPIResult(_ arg: String) -> FlutterError {
+		return FlutterError(code: "-1", message: "\(arg) is null.", details: nil)
+	}
+	
+	internal static let unKnownError = FlutterError(code: "-1", message: "This code shouldn't get executed", details: nil)
 }
--- ios/Classes/SwiftVirtusizeFlutterPlugin.swift
+++ ios/Classes/SwiftVirtusizeFlutterPlugin.swift
@@ -4,11 +4,27 @@ import Virtusize
 
 public class SwiftVirtusizeFlutterPlugin: NSObject, FlutterPlugin {
 	
+	private var flutterChannel: FlutterMethodChannel?
+	
 	private let repository = VirtusizeFlutterRepository.shared
+	private var workItem: DispatchWorkItem?
+	private var product: VirtusizeProduct?
+	private var productCheckData: [String: Any]?
+	private var storeProduct: VirtusizeStoreProduct? = nil
+	private var productTypes: [VirtusizeProductType]? = nil
+	private var i18nLocalization: VirtusizeI18nLocalization? = nil
+	private var userSessionResponse: String? = ""
+	private var userProducts: [VirtusizeStoreProduct]? = nil
+	private var bodyProfileRecommendedSize: BodyProfileRecommendedSize? = nil
+	private var selectedUserProductId: Int? = nil
+
+	init(channel: FlutterMethodChannel) {
+		flutterChannel = channel
+	}
 	
 	public static func register(with registrar: FlutterPluginRegistrar) {
 		let channel = FlutterMethodChannel(name: "com.virtusize/virtusize_flutter_plugin", binaryMessenger: registrar.messenger())
-		let instance = SwiftVirtusizeFlutterPlugin()
+		let instance = SwiftVirtusizeFlutterPlugin(channel: channel)
 		registrar.addMethodCallDelegate(instance, channel: channel)
 	}
 	
@@ -16,15 +32,15 @@ public class SwiftVirtusizeFlutterPlugin: NSObject, FlutterPlugin {
 		switch call.method {
 			case "setVirtusizeProps":
 				guard let arguments = call.arguments as? [String: Any] else {
-					result(FlutterMethodNoArguments)
+					result(FlutterError.noArguments)
 					return
 				}
 				if let apiKey = arguments["apiKey"] as? String {
 					Virtusize.APIKey = apiKey
 				} else {
-					result(FlutterMethodNotSetArgument("apiKey"))
+					result(FlutterError.argumentNotSet("apiKey"))
 				}
-				
+
 				if let userID = arguments["externalUserId"] as? String {
 					Virtusize.userID = userID
 				}
@@ -66,7 +82,7 @@ public class SwiftVirtusizeFlutterPlugin: NSObject, FlutterPlugin {
 				Virtusize.params = virtusizeBuilder.build()
 			case "getProductDataCheck":
 				guard let arguments = call.arguments as? [String: Any] else {
-					result(FlutterMethodNoArguments)
+					result(FlutterError.noArguments)
 					return
 				}
 				guard let productId = arguments["externalId"] as? String   else {
@@ -80,22 +96,152 @@ public class SwiftVirtusizeFlutterPlugin: NSObject, FlutterPlugin {
 				}
 				
 				DispatchQueue.main.async {
-					let pdcJsonString = self.repository.getProductDataCheck(
+					var pdcJsonString: String? = nil
+					(self.product, pdcJsonString) = self.repository.getProductDataCheck(
 						product:
 							VirtusizeProduct(
 								externalId: productId,
 								imageURL: imageURL
 							)
 					)
+					self.productCheckData = self.product?.dictionary["data"] as? [String: Any]
 					result(pdcJsonString)
 				}
 			case "openVirtusizeWebView":
-				if let viewController = VirtusizeWebViewController(processPool: Virtusize.processPool) {
+				if let viewController = VirtusizeWebViewController(
+					product: product,
+					userSessionResponse: userSessionResponse,
+					eventHandler: self,
+					processPool: Virtusize.processPool) {
 					let flutterRootViewController = UIApplication.shared.windows.first?.rootViewController
 					flutterRootViewController?.present(viewController, animated: true)
 				}
+			case "getRecommendationText":
+				guard let storeProductId = self.productCheckData?["productDataId"] as? Int else {
+					result(FlutterError.unKnownError)
+					return
+				}
+				workItem = DispatchWorkItem {
+					self.fetchInitialData(result, storeProductId: storeProductId)
+					self.updateUserSession(result)
+					self.getRecommendation(result)
+				}
+				DispatchQueue.main.async(execute: workItem!)
 			default:
 				result(FlutterMethodNotImplemented)
 		}
+	}
+	
+	private func fetchInitialData(_ result: @escaping FlutterResult, storeProductId: Int) {
+		storeProduct = repository.getStoreProduct(productId: storeProductId)
+		if storeProduct == nil {
+			result(FlutterError.nullAPIResult("storeProduct"))
+			workItem?.cancel()
+		}
+		
+		productTypes = repository.getProductTypes()
+		if productTypes == nil {
+			result(FlutterError.nullAPIResult("productTypes"))
+			workItem?.cancel()
+		}
+
+		i18nLocalization = repository.getI18nLocalization()
+		if i18nLocalization == nil {
+			result(FlutterError.nullAPIResult("i18nLocalization"))
+			workItem?.cancel()
+		}
+	}
+	
+	private func updateUserSession(_ result: FlutterResult? = nil) {
+		userSessionResponse = repository.getUserSessionResponse()
+		if userSessionResponse == nil {
+			if let result = result {
+				result(FlutterError.nullAPIResult("userSessionResponse"))
+			} else {
+				flutterChannel?.invokeMethod("onRecTextChange", arguments: nil)
+			}
+			workItem?.cancel()
+		}
+	}
+	
+	private func getRecommendation(
+		_ result: FlutterResult? = nil,
+		selectedRecommendedType: SizeRecommendationType? = nil,
+		shouldUpdateUserProducts: Bool = true,
+		shouldUpdateUserBodyProfile: Bool = true
+	) {
+		if shouldUpdateUserProducts {
+			userProducts = repository.getUserProducts()
+			if userProducts == nil {
+				if let result = result {
+					result(FlutterError.nullAPIResult("userProducts"))
+				} else {
+					flutterChannel?.invokeMethod("onRecTextChange", arguments: nil)
+				}
+				workItem?.cancel()
+			}
+		}
+
+		if shouldUpdateUserBodyProfile {
+			let userBodyProfile = repository.getUserBodyProfile()
+			bodyProfileRecommendedSize = (userBodyProfile != nil) ?
+				repository.getBodyProfileRecommendedSize(
+					productTypes: productTypes!,
+					storeProduct: storeProduct!,
+					userBodyProfile: userBodyProfile!
+				)
+				: nil
+		}
+		
+
+		let recText = repository.getRecommendationText(
+			selectedRecType: selectedRecommendedType,
+			userProducts: (selectedUserProductId != nil) ?
+				userProducts?.filter { $0.id == selectedUserProductId } : userProducts,
+			storeProduct: storeProduct!,
+			productTypes: productTypes!,
+			bodyProfileRecommendedSize: bodyProfileRecommendedSize,
+			i18nLocalization: i18nLocalization!
+		)
+		
+		if let result = result {
+			result(recText)
+		} else {
+			flutterChannel?.invokeMethod("onRecTextChange", arguments: recText)
+		}
+	}
+}
+
+extension SwiftVirtusizeFlutterPlugin: VirtusizeEventHandler {
+	public func userOpenedWidget() {
+		
+	}
+	
+	public func userAuthData(bid: String?, auth: String?) {
+		
+	}
+	
+	public func userLoggedIn() {
+		
+	}
+	
+	public func clearUserData() {
+		
+	}
+	
+	public func userSelectedProduct(userProductId: Int?) {
+		
+	}
+	
+	public func userAddedProduct(userProductId: Int?) {
+		
+	}
+	
+	public func userUpdatedBodyMeasurements(recommendedSize: String?) {
+		
+	}
+	
+	public func userChangedRecommendationType(changedType: SizeRecommendationType?) {
+		
 	}
 }
