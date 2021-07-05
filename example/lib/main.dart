8051e9a6287ba42c86bc7e3e19092bcbfc783b52
Kuei <akueisara39@gmail.com>
2021-07-06 00:42:17 +0900

    Tweak the layout for the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,7 +35,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
 
     VirtusizePlugin.instance.setProduct(
-      // Set the product's external ID
+        // Set the product's external ID
         externalId: '694',
         // Set the product image URL
         imageUrl: 'http://www.image.com/goods/12345.jpg');
@@ -53,25 +53,29 @@ class _MyAppState extends State<MyApp> {
                   padding: EdgeInsets.only(top: 16),
                   child: Column(mainAxisSize: MainAxisSize.min, children: [
                     VirtusizeButton.vsStyle(),
+                    Container(height: 16),
                     VirtusizeButton.vsStyle(
                         style: VirtusizeStyle.Teal, child: Text("Custom Text")),
+                    Container(height: 16),
                     VirtusizeButton(
                       child: ElevatedButton.icon(
-                          label: Text('サイズチェック'),
+                          label: Text('サイズチェック', style: TextStyle(fontSize: 12)),
                           icon: Icon(Icons.account_circle_rounded),
                           style: ElevatedButton.styleFrom(
                               primary: Color(0xFF191919),
                               padding: EdgeInsets.symmetric(
-                                  horizontal: 12, vertical: 10),
+                                  horizontal: 10, vertical: 8),
                               shape: RoundedRectangleBorder(
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(32.0)))),
                           onPressed:
                               VirtusizePlugin.instance.openVirtusizeWebView),
                     ),
+                    Container(height: 16),
                     VirtusizeButton(
                       child: Text("Custom Text"),
                     ),
+                    Container(height: 16),
                     VirtusizeInPageMini()
                   ])))),
     );
