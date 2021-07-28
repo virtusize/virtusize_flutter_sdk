4bd107b70bef5d08897f4128403a062669e515fa
Kuei <akueisara39@gmail.com>
2021-07-28 14:29:55 +0900

    Update FlutterActivity to FlutterFragmentActivity in the example app

--- example/android/app/src/main/kotlin/com/example/virtusize_flutter_sdk_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/virtusize_flutter_sdk_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.virtusize.virtusize_flutter_sdk_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
