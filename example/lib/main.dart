1dc78a89888a3f41c8db14a379cd8a20310d12b5
Mikhail Yevstratiev <mikhail.yevstratiev@diversido.io>
2025-03-28 15:45:39 +0200

    Update flutter example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,24 +5,24 @@ import 'package:virtusize_flutter_sdk/virtusize_flutter_sdk.dart';
 import 'screens/home_screen.dart';
 
 Future<void> main() async {
-
   /// Add the following code before calling runApp
   WidgetsFlutterBinding.ensureInitialized();
-  VirtusizeSDK.instance.setVirtusizeParams(
-      // Only the API key is required
-      apiKey: '15cc36e1d7dad62b8e11722ce1a245cb6c5e6692',
-      // For using the Order API, a user ID is also required. (can be set later)
-      userId: '123',
-      // By default, the Virtusize environment will be set to VSEnvironment.global
-      env: VSEnvironment.staging,
-      // By default, the initial language will be set according to the Virtusize environment
-      language: VSLanguage.jp,
-      // By default, ShowSGI is false
-      showSGI: true,
-      // By default, Virtusize allows all possible languages
-      allowedLanguages: [VSLanguage.en, VSLanguage.jp],
-      // By default, Virtusize displays all possible info categories in the Product Details tab
-      detailsPanelCards: [VSInfoCategory.generalFit, VSInfoCategory.brandSizing]);
+  await VirtusizeSDK.instance.setVirtusizeParams(
+    // Only the API key is required
+    apiKey: '15cc36e1d7dad62b8e11722ce1a245cb6c5e6692',
+    // For using the Order API, a user ID is also required. (can be set later)
+    userId: '123',
+    // By default, the Virtusize environment will be set to VSEnvironment.global
+    env: VSEnvironment.staging,
+    // By default, the initial language will be set according to the Virtusize environment
+    language: VSLanguage.jp,
+    // By default, ShowSGI is false
+    showSGI: true,
+    // By default, Virtusize allows all possible languages
+    allowedLanguages: [VSLanguage.en, VSLanguage.jp],
+    // By default, Virtusize displays all possible info categories in the Product Details tab
+    detailsPanelCards: [VSInfoCategory.generalFit, VSInfoCategory.brandSizing],
+  );
 
   runApp(MyApp());
 }
@@ -31,8 +31,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        /// Refer to the code in the HomeScreen to see how to set up your product info and the Virtusize widgets
-        home: HomeScreen()
+      /// Refer to the code in the HomeScreen to see how to set up your product info and the Virtusize widgets
+      home: HomeScreen(),
     );
   }
 }
--- example/lib/screens/product_screen.dart
+++ example/lib/screens/product_screen.dart
@@ -5,9 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:virtusize_flutter_sdk/virtusize_flutter_sdk.dart';
 
 class ProductScreen extends StatefulWidget {
-  final String externalID;
+  final String? externalID;
 
-  ProductScreen({this.externalID});
+  const ProductScreen({super.key, this.externalID});
 
   @override
   State<StatefulWidget> createState() => _ProductScreenState();
@@ -22,22 +22,24 @@ class _ProductScreenState extends State<ProductScreen> {
     "vs_jacket",
     "vs_sweater",
     "vs_skirt",
-    "vs_pants"
+    "vs_pants",
   ];
 
-  VirtusizeClientProduct _product;
-  String _externalID;
+  late final VirtusizeClientProduct _product;
+  late final String _externalID;
 
   @override
   void initState() {
     super.initState();
 
-    _externalID = widget.externalID ??
+    _externalID =
+        widget.externalID ??
         _externalIDList[Random().nextInt(_externalIDList.length)];
 
     _product = VirtusizeClientProduct(
-        externalProductId: _externalID,
-        imageURL: 'https://www.image.com/goods/12345.jpg');
+      externalProductId: _externalID,
+      imageURL: 'https://www.image.com/goods/12345.jpg',
+    );
 
     VirtusizeSDK.instance.loadVirtusize(_product);
   }
@@ -45,27 +47,39 @@ class _ProductScreenState extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Virtusize Product $_externalID'),
-        ),
-        body: Center(
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          VirtusizeInPageStandard.vsStyle(
-              product: _product, style: VirtusizeStyle.Black),
-          Container(height: 16),
-          VirtusizeInPageMini.vsStyle(
-              product: _product, style: VirtusizeStyle.Teal),
-          Container(height: 16),
-          VirtusizeButton.vsStyle(product: _product),
-          Container(height: 16),
-          ElevatedButton(
+      appBar: AppBar(title: Text('Virtusize Product $_externalID')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            VirtusizeInPageStandard.vsStyle(
+              product: _product,
+              style: VirtusizeStyle.Black,
+            ),
+            Container(height: 16),
+            VirtusizeInPageMini.vsStyle(
+              product: _product,
+              style: VirtusizeStyle.Teal,
+            ),
+            Container(height: 16),
+            VirtusizeButton.vsStyle(product: _product),
+            Container(height: 16),
+            ElevatedButton(
               onPressed: () {
-                Navigator.push(context, CupertinoPageRoute(builder: (_) {
-                  return ProductScreen();
-                }));
+                Navigator.push(
+                  context,
+                  CupertinoPageRoute(
+                    builder: (_) {
+                      return ProductScreen();
+                    },
+                  ),
+                );
               },
-              child: Text("Go to next product page"))
-        ])));
+              child: Text("Go to next product page"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
